8b24aee41f79d4da76aed98dbe0ca3626b481978
puntonim <puntonim@gmail.com>
2025-10-29 17:58:39 +0100

    docs(settings-utils): better docstrings

--- settings-utils/settings_utils/settings_utils.py
+++ settings-utils/settings_utils/settings_utils.py
@@ -54,6 +54,11 @@ def get_string_from_env_or_file(
     default: str | None = _DEFAULT,
     is_env_string_json=False,
 ):
+    """
+    Warning: storing secrets as plain-text in local files is not recommended.
+    Check out `get_string_from_env_or_aws_parameter_store()`, instead, and store your
+     secrets in AWS Parameter Store.
+    """
     try:
         value = get_string_from_env(env_key)
     except KeyError:
@@ -77,21 +82,6 @@ def get_string_from_env_or_file(
     return value
 
 
-def copy_settings(from_, to_):
-    """
-    Copy all settings from a class to another.
-    Typically used to copy `test_settings` to `settings` in `pytest.py`.
-    """
-    attr_names = [
-        attr
-        for attr in dir(from_)
-        if not callable(getattr(from_, attr)) and not attr.startswith("__")
-    ]
-    for attr_name in attr_names:
-        attr_value = getattr(from_, attr_name)
-        setattr(to_, attr_name, attr_value)
-
-
 def get_string_from_env_or_aws_parameter_store(
     env_key: str,
     param_store_key_path: str | Path,
@@ -101,7 +91,19 @@ def get_string_from_env_or_aws_parameter_store(
     do_skip_param_store_cache=False,
 ):
     """
-     Note: this fn is available only if pip-installed with the extra:
+    Read a string from env vars (eg. when running in AWS Lambda) or from
+     AWS Param Store (eg. in dev or when recording tests, which is better than using
+     a local file with the secret in plain-text).
+
+    Mind that the actual settings' attribute needs to be a @property for lazily
+     evaluation, so vcr.py can catch it on time.
+    And that in order to use @property you have to make `settings` an instance,
+     unlike other projects where `settings` is a class.
+    And also, mind that in conftest.py you have to use the fixture clear_cache_for_aws_param_store_client()
+     to clear AWS Param Store client cache, to make its HTTP interactions
+     deterministic (otherwise vcr.py fails).
+
+    Note: this fn is available only if pip-installed with the extra:
      pip install settings-utils[get-from-aws-param-store]
 
     Args:
@@ -111,6 +113,42 @@ def get_string_from_env_or_aws_parameter_store(
         is_value_json: True for JSON value.
         param_store_cache_ttl: time-to-live for the Python in-memory cached params from AWS Param Store, seconds.
         do_skip_param_store_cache: skip the Python in-memory cache for AWS Param Store.
+
+    Example, from https://github.com/puntonim/botte-monorepo/tree/main/projects/botte-be:
+        File: conf/settings.py
+        ```py
+        import settings_utils
+
+        class _Settings:
+            APP_NAME = "Botte BE"
+            IS_TEST = False
+            DO_ENABLE_API_AUTHORIZER = True
+            API_AUTHORIZER_TOKEN = settings_utils.get_string_from_env(
+                "API_AUTHORIZER_TOKEN", "XXX"
+            )
+
+            @property
+            def TELEGRAM_TOKEN(self):
+                return settings_utils.get_string_from_env_or_aws_parameter_store(
+                    env_key="TELEGRAM_TOKEN",
+                    param_store_key_path="/botte-be/prod/telegram-token",
+                    default="XXX",
+                    param_store_cache_ttl=60,
+                )
+
+        class _TestSettings:
+            IS_TEST = True
+
+        settings = _Settings()
+        ```
+
+        File: tests/conftest.py
+        ```py
+        ...
+        @pytest.fixture(autouse=True, scope="function")
+        def clear_cache_for_aws_param_store_client():
+            aws_parameter_store_client.cache.clear_cache()
+        ```
     """
     try:
         # Since this requires an extra req, dynamically import requirements.
@@ -150,3 +188,18 @@ def get_string_from_env_or_aws_parameter_store(
             raise KeyError
         value = default
     return value
+
+
+def copy_settings(from_, to_):
+    """
+    Copy all settings from a class to another.
+    Typically used to copy `test_settings` to `settings` in `pytest.py`.
+    """
+    attr_names = [
+        attr
+        for attr in dir(from_)
+        if not callable(getattr(from_, attr)) and not attr.startswith("__")
+    ]
+    for attr_name in attr_names:
+        attr_value = getattr(from_, attr_name)
+        setattr(to_, attr_name, attr_value)
