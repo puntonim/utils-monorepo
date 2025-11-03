863ed2ee8554eb2178a5a2f739a3ab31bf6b1a4e
puntonim <puntonim@gmail.com>
2025-11-03 11:15:22 +0100

    feat(json-utils): prettify_to_non_json_string()

--- json-utils/json_utils/json_utils.py
+++ json-utils/json_utils/json_utils.py
@@ -6,34 +6,59 @@ import json
 import json_utils
 
 data = {"date": datetime(2025, 1, 1)}
-json_utils.to_json(data)
+json_utils.to_json_string(data, indent=4)
 
 json.dumps(data, cls=json_utils.CustomJsonEncoder)
 ```
 """
 
 import json
+import warnings
 from datetime import datetime
+from typing import Any
 from uuid import UUID
 
 # Objects exported to the `import *` in `__init__.py`.
 __all__ = [
     "CustomJsonEncoder",
+    "to_json_string",
     "to_json",
+    "prettify_to_non_json_string",
 ]
 
 
-def to_json(data, sort_keys=False, **kwargs) -> str:
+def to_json_string(data: Any, sort_keys=False, **kwargs) -> str:
     """
+    Convert a Python object to a JSON string.
+    The advantage, compared to a plain json.dumps(), is that it can handle types like
+     datetime, bytes, UUID, that would raise a TypeError with a plain json.dumps().
+
+    Args:
+        data: any Python object.
+        sort_keys: True to sort keys in the JSON string (if the JSON is a map or list).
+        **kwargs: passed down to json.dumps(...), eg. `indent=4`.
+
     Usage:
         data = {"date": datetime(2025, 1, 1)}
-        json_utils.to_json(data)
+        json_utils.to_json(data, indent=4)
     """
     return json.dumps(data, cls=CustomJsonEncoder, sort_keys=sort_keys, **kwargs)
 
 
+def to_json(*args, **kwargs) -> str:
+    """
+    ** DEPRECATED **
+    Use to_json_string() instead, which is exactly the same but with a better name.
+    """
+    warnings.warn("to_json() is deperecated; use to_json_string()", DeprecationWarning)
+    return to_json_string(*args, **kwargs)
+
+
 class CustomJsonEncoder(json.JSONEncoder):
     """
+    A custom JSON encoder that can handle types like datetime, bytes, UUID, etc,
+     unlike the std lib default encoder.
+
     Usage:
         import json
         data = {"date": datetime(2025, 1, 1)}
@@ -60,3 +85,45 @@ class CustomJsonEncoder(json.JSONEncoder):
         elif isinstance(obj, UUID):
             return str(obj)
         return json.JSONEncoder.default(self, obj)
+
+
+def prettify_to_non_json_string(text: str):
+    """
+    Take a string (likely a JSON string) and performs char replacements
+     like "\\n" to "\n" and similar.
+
+    This is useful when the input JSON string is an object (a dict) containing strings
+     that contains fi. new lines encoded as "\\n" (because you cannot use an actual
+     new line inside a string in JSON). This function replaces "\\n" with an actual
+     new line, so making the string not a JSON string anymore, but more
+     human-readable.
+
+    Example:
+        text = \"""
+            {
+                "id": "39296166250439623962212092407450232854575369122363277321",
+                "timestamp": 1762101037459,
+                "message": "[ERROR] UnhealthEndpointException\nTraceback (most recent call last):\n\u00a0\u00a0File \"/opt/python/aws_lambda_powertools/logging/logger.py\", line 548, in decorate\n\u00a0\u00a0\u00a0\u00a0return lambda_handler(event, context, *args, **kwargs)\n\u00a0\u00a0File \"/var/task/botte_be/views/endpoint_introspection_view.py\", line 170, in lambda_handler\n\u00a0\u00a0\u00a0\u00a0raise UnhealthEndpointException(ts=now)"
+            }
+        \"""
+
+        text = prettify_to_non_json_string(text)
+        \"""
+        {
+            "id": "39296166250439623962212092407450232854575369122363277321",
+            "timestamp": 1762101037459,
+            "message": "[ERROR] UnhealthEndpointException
+        Traceback (most recent call last):
+          File "/opt/python/aws_lambda_powertools/logging/logger.py", line 548, in decorate
+            return lambda_handler(event, context, *args, **kwargs)
+          File "/var/task/botte_be/views/endpoint_introspection_view.py", line 170, in lambda_handler
+            raise UnhealthEndpointException(ts=now)"
+        }
+        \"""
+    """
+    text = (
+        text.replace("\\n", "\n")  # \n -> new line.
+        .replace("\\u00a0", " ")  # \\u00a0 -> space.
+        .replace('\\"', '"')  # \\" -> ".
+    )
+    return text
--- json-utils/tests/test_json_utils.py
+++ json-utils/tests/test_json_utils.py
@@ -6,12 +6,28 @@ import pytest
 import json_utils
 
 
-class TestToJson:
+class TestToJsonString:
     def test_happy_flow(self):
         data = {"date": datetime(2025, 1, 1)}
         with pytest.raises(TypeError):
             json.dumps(data)
-        assert json_utils.to_json(data)
+        assert json_utils.to_json_string(data)
+
+
+class TestPrettifyToNonJsonString:
+    def test_happy_flow(self):
+        data = {
+            "id": "39296166250439623962212092407450232854575369122363277321",
+            "timestamp": 1762101037459,
+            "message": '[ERROR] UnhealthEndpointException\nTraceback (most recent call last):\n\u00a0\u00a0File "/opt/python/aws_lambda_powertools/logging/logger.py", line 548, in decorate\n\u00a0\u00a0\u00a0\u00a0return lambda_handler(event, context, *args, **kwargs)\n\u00a0\u00a0File "/var/task/botte_be/views/endpoint_introspection_view.py", line 170, in lambda_handler\n\u00a0\u00a0\u00a0\u00a0raise UnhealthEndpointException(ts=now)',
+        }
+        text = json_utils.to_json_string(data, indent=4)
+        assert "\\n" in text
+        print("\n" + text)
+
+        text = json_utils.prettify_to_non_json_string(text)
+        print("\n" + text)
+        assert "\\n" not in text
 
 
 class TestCustomJsonEncoder:
