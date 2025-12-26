1b1572aa51852a32b1bbaa758fea90675a3b8837
puntonim <puntonim@gmail.com>
2025-12-26 22:46:34 +0100

    feat(speed-utils): functions that revert the existing unit conversions

--- speed-utils/speed_utils/speed_utils.py
+++ speed-utils/speed_utils/speed_utils.py
@@ -16,14 +16,19 @@ __all__ = [
     "minpkm_base10_to_base60",
     "mps_to_minpkm_base10",
     "mps_to_kmph",
+    "minpkm_base60_to_base10",
+    "minpkm_base10_to_mps",
+    "kmph_to_mps",
 ]
 
 
-def minpkm_base10_to_base60(x: float) -> str:
+def minpkm_base10_to_base60(x: float | int) -> str:
     """
     Eg. 5.05 min/km -> 5:03 min/km.
         minpkm_base10_to_base60(5.05) -> "5:03".
     """
+    if not isinstance(x, (float, int)):
+        raise ValueError("only float arg supported")
     int_val = floor(x)
     dec_val = round((x - int_val) * 60)
     if dec_val == 60:
@@ -32,18 +37,62 @@ def minpkm_base10_to_base60(x: float) -> str:
     return f"{int_val}:{dec_val:02d}"
 
 
-def mps_to_minpkm_base10(x: float) -> float:
+def minpkm_base60_to_base10(x: str) -> float:
+    """
+    Eg. 5:03 min/km -> 5.05 min/km.
+        minpkm_base60_to_base10("5:03") -> 5.05.
+    """
+    exc_msg = 'only str arg like "5:03" supported'
+    if not isinstance(x, str):
+        raise ValueError(exc_msg)
+    tokens = x.split(":")
+    if len(tokens) != 2:
+        raise ValueError(exc_msg)
+    try:
+        minute = int(tokens[0])
+        second = int(tokens[1])
+    except ValueError as exc:
+        raise ValueError(exc_msg) from exc
+
+    return round(minute + second / 60, 2)
+
+
+def mps_to_minpkm_base10(x: float | int) -> float:
     """
     Eg. 3.3 m/s -> 5.05 min/km.
-        mps_to_minpkm(3.3) -> 5.05
+        mps_to_minpkm_base10(3.3) -> 5.05
     Note that 5.05 min/km could then be converted to base60 to 5:03 min/km.
     """
+    if not isinstance(x, (float, int)):
+        raise ValueError("only float arg supported")
     return 60 / (x * 3.6)
 
 
-def mps_to_kmph(x: float) -> float:
+def minpkm_base10_to_mps(x: float | int) -> float:
+    """
+    Eg. 5.05 min/km -> 3.3 m/s.
+        minpkm_base10_to_mps(5.05) -> 3.3
+    """
+    if not isinstance(x, (float, int)):
+        raise ValueError("only float arg supported")
+    return 60 / (x * 3.6)
+
+
+def mps_to_kmph(x: float | int) -> float:
     """
     Eg. 4.344 m/s -> 15.6384 min/km.
         mps_to_kmph(4.344) -> 15.6384
     """
+    if not isinstance(x, (float, int)):
+        raise ValueError("only float arg supported")
     return x * 3.6
+
+
+def kmph_to_mps(x: float | int) -> float:
+    """
+    Eg. 15.6384 min/km -> 4.344 m/s.
+        kmph_to_mps(15.6384) -> 4.344 m/s
+    """
+    if not isinstance(x, (float, int)):
+        raise ValueError("only float arg supported")
+    return x / 3.6
--- speed-utils/tests/test_speed_utils.py
+++ speed-utils/tests/test_speed_utils.py
@@ -1,7 +1,7 @@
 import speed_utils
 
 
-class TestMinPKmBase10ToBase60:
+class Test_minpkm_base10_to_base60:
     def test_happy_flow(self):
         assert speed_utils.minpkm_base10_to_base60(5.05) == "5:03"
 
@@ -12,6 +12,38 @@ class TestMinPKmBase10ToBase60:
         assert speed_utils.minpkm_base10_to_base60(4.0001) == "4:00"
 
 
-class TestMpsToKmph:
+class Test_minpkm_base60_to_base10:
+    def test_happy_flow(self):
+        assert speed_utils.minpkm_base60_to_base10("5:03") == 5.05
+
+    def test_approx_60_secs(self):
+        assert speed_utils.minpkm_base60_to_base10("5:00") == 5.0
+        assert speed_utils.minpkm_base60_to_base10("4:59") == 4.98
+        assert speed_utils.minpkm_base60_to_base10("5:01") == 5.02
+
+    def test_approx_0_secs(self):
+        assert speed_utils.minpkm_base60_to_base10("4:00") == 4.0
+        assert speed_utils.minpkm_base60_to_base10("3:59") == 3.98
+        assert speed_utils.minpkm_base60_to_base10("4:01") == 4.02
+
+
+class Test_mps_to_minpkm_base10:
+    def test_happy_flow(self):
+        assert round(speed_utils.mps_to_minpkm_base10(3.3), 2) == 5.05
+        assert round(speed_utils.mps_to_minpkm_base10(5.7), 2) == 2.92
+
+
+class Test_minpkm_base10_to_mps:
+    def test_happy_flow(self):
+        assert round(speed_utils.minpkm_base10_to_mps(5.05), 2) == 3.30
+        assert round(speed_utils.minpkm_base10_to_mps(2.92), 2) == 5.71
+
+
+class Test_mps_to_kmph:
     def test_happy_flow(self):
         assert speed_utils.mps_to_kmph(4.344) == 15.6384
+
+
+class Test_kmph_to_mps:
+    def test_happy_flow(self):
+        assert speed_utils.kmph_to_mps(15.6384) == 4.344
